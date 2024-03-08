6b03a75636a5a30a8488e2269796a2d0b44895ca
Prakharvermavr <159545893+Prakharvermavr@users.noreply.github.com>
2024-03-09 02:22:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration file for DDoS prevention tool
+
+# TCP protocol configurations
+TCP_ENABLED = True  # Set to True to enable TCP protection
+TCP_THRESHOLD = 100  # Maximum allowed TCP connections per second
+
+# UDP protocol configurations
+UDP_ENABLED = True  # Set to True to enable UDP protection
+UDP_THRESHOLD = 50  # Maximum allowed UDP packets per second
